c846c8fa864b27fe1637a659c47ed95b42357f5d
VDGone <disasterpiece788@gmail.com>
2017-03-10 12:16:59 +0200

    practice

--- SelectionSort/Program.cs
+++ SelectionSort/Program.cs
@@ -1,15 +1,32 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SelectionSort
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            int[] numsToSort = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int temp;
+
+            if (numsToSort.Length > 1)
+            {
+                for (int i = 0; i < numsToSort.Length - 1; i++)
+                {
+                    for (int j = i + 1; j < numsToSort.Length; j++)
+                    {
+                        if (numsToSort[i] > numsToSort[j])
+                        {
+                            temp = numsToSort[i];
+                            numsToSort[i] = numsToSort[j];
+                            numsToSort[j] = temp;
+                        }
+                    }
+                }
+            }
+
+            Console.WriteLine(string.Join(", ", numsToSort));
         }
     }
 }
